a89710fbfc0a7acfbab0ce5006fb580db69b8735
Steph Blain <blain@zoology.ubc.ca>
2024-02-15 13:28:12 -0600

    add script to check output

---
+++ HummerHybrids_check.R
@@ -0,0 +1,38 @@
+library(tidyverse); library(data.table)
+
+setwd("/scratch/user/sblain/hummer_aims/run_hmm_autosomes/")
+
+hybs<-fread("HummerHybrids_posterior_states",sep="\t",header=T)
+srrIDs<-colnames(hybs)[startsWith(colnames(hybs),"SRR")]
+hybsRef<-hybs%>%select(1:2,all_of(srrIDs))
+
+SAMPLES<-c("rubythroated_SRR12247318", "blackchinned_SRR12247328", "blackchinned_SRR12247340", "blackchinned_SRR12247342", "blackchinned_SRR12247344", "blackchinned_SRR12247345", "blackchinned_SRR12247346", "blackchinned_SRR12247347", "blackchinned_SRR12247348", "rubythroated_SRR12247319", "rubythroated_SRR12247320", "rubythroated_SRR12247322", "rubythroated_SRR12247325", "rubythroated_SRR12247326", "rubythroated_SRR12247327", "rubythroated_SRR12247324", "blackchinned_SRR12247329", "rubythroated_SRR12247323")
+spIDs<-data.frame(species=str_split_i(SAMPLES,"_",1),
+           reference=str_split_i(SAMPLES,"_",2))%>%
+  filter(reference%in%srrIDs)
+
+hybsRef<-hybsRef%>%
+  mutate(ChromPos=paste(CHROM,POS,sep="_"))%>%select(-CHROM,-POS)%>%
+  pivot_longer(cols=c(-ChromPos),names_to = "reference",values_to="genotype")%>%
+  left_join(spIDs)
+
+highNAs<-hybsRef%>%group_by(ChromPos)%>%
+  summarise(NAs=sum(is.na(genotype))/18)%>%
+  filter(NAs>0.25)%>%pull(ChromPos)
+
+hybsRef<-hybsRef%>%filter(!ChromPos%in%highNAs)
+
+hybsRef%>%group_by(species)%>%
+  summarise(species_mean=mean(genotype,na.rm=T))
+
+parhmm<-fread("../ref_panel/HummerRefPanel.f50.parhmm")
+colnames(parhmm)<-c("CHROM","POS","refSp1","altSp1","refSp2","altSp2")
+parhmm<-parhmm%>%mutate(ChromPos=paste(CHROM,POS,sep="_"))%>%select(-CHROM,-POS)%>%
+  filter(ChromPos%in%unique(hybsRef$ChromPos))
+
+parhmm<-parhmm%>%mutate(refSp=if_else(refSp1/(refSp1+altSp1)>refSp2/(refSp2+altSp2),"sp1","sp2"))
+
+hybsRef<-hybsRef%>%left_join(parhmm%>%select(ChromPos,refSp))
+
+hybsRef%>%group_by(species,refSp)%>%
+  summarise(species_mean=mean(genotype,na.rm=T))
--- merge_dfs.py
+++ merge_dfs.py
@@ -1,55 +1,76 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-#Combine input data for AncestryHMM counts file
 
 import pandas as pd
 import sys
 
-#collect all command line arguments
+
 projectName=sys.argv[1] #"HummerHybrids"
 f_tag=sys.argv[2] #"f50"
 aimsFile=sys.argv[3] #"HummerRefPanel.f50.noUnderscores.aims"
 aims_cmsFile=sys.argv[4] #"HummerRefPanel.f50.noUnderscores.aims.cMs"
 parhmmFile=sys.argv[5] #"HummerRefPanel.f50.parhmm"
-hybridCountsFile=sys.argv[6] #"../HummerHybrids.f50.readCounts"
-indListFile=sys.argv[7] #"../HummerHybrids.f50.readCounts.ind"
+hybridCountsFile=sys.argv[6] #"../HummerHybrids.f50.hmmFormat"
+indsFile=sys.argv[7] #"../HummerHybrids.f50.hmmFormat.ind2"
 
-#read in files
+
+
+############################################################
+#read in data files and rename columns
+############################################################
+
 aims=pd.read_csv(aimsFile,header=None,sep="\t")
 aims_cms=pd.read_csv(aims_cmsFile,header=None,sep="\t")
 parhmm=pd.read_csv(parhmmFile,header=None,sep="\t")
-hybCounts=pd.read_csv(hybridCountsFile,header=None,sep="\t")
-indList=pd.read_csv(indListFile,header=None,sep="\t")
+inds=pd.read_csv(indsFile,header=None,sep="\t")
+counts=pd.read_csv(hybridCountsFile,header=None,sep="\t")
 
-#rename columns; all df's need a "chr" and "pos" column
+
 aims.columns=["chr","pos","Ref","Alt"]
 aims_cms.columns=["chr","pos","Ref","Alt","cMs"]
 parhmm.columns=["chr","pos","sp1ref","sp1alt","sp2ref","sp2alt"]
-hybCols=['chr','pos'] #first two cols are chromosome and position
-#duplicate individual names (ind1, ind2 > ind1, ind1, ind2, ind2)
-hybCols.extend([val for val in list(indList[0]) for _ in (0, 1)])
-hybCounts.columns=hybCols
 
-#left join so that everything aligns with aims file
-aims=aims.merge(parhmm,how="left").merge(aims_cms,how="left").merge(hybCounts,how="left")
+hybCols=['chr','pos','Ref','Alt'] #first two cols are chromosome and position
+#duplicate individual names (ind1, ind2 > ind1, ind1, ind2, ind2)
+hybCols.extend([val for val in list(inds[0]) for _ in (0, 1)])
+counts.columns=hybCols
+#counts.head(10)
+
+
+############################################################
+#merge all dataframes, drop ref and alt nucleotide columns
+############################################################
+
+aims=aims.merge(parhmm,how="left").merge(aims_cms,how="left").merge(counts,how="left")
 
 aims=aims.drop(["Ref","Alt"],axis=1)
+aims.head(10)
 
-#print("first few lines of aims + parents + cms")
-#aims.head(10)
 
-print("NAs per column")
-aimsNAs=aims.isna().sum()
-print(aimsNAs)
+#check that the number of rows in the output make sense
+print("Number of rows in output")
+print(aims.shape[0])
 
-indList['ploidy']="2" #declare all individuals to be diploid
+print("Number of rows in cMs input")
+print(aims_cms.shape[0])
+
+#aims['chr'].unique()
+
+
+inds['ploidy']="2" #declare all individuals to be diploid
+
+
+############################################################
+#write output files
+############################################################
+
 
 outFile="ancestryHMM_counts_"+projectName+"_"+f_tag
 aims.to_csv(outFile,index=False,header=False,sep="\t",float_format='%.15f')
 print("counts output printed to: "+outFile)
 
 outFile2="ancestryHMM_samples_"+projectName+"_"+f_tag
-indList.to_csv(outFile2,index=False,header=False,sep="\t")
+inds.to_csv(outFile2,index=False,header=False,sep="\t")
 print("samples output printed to: "+outFile2)
 
